a8f793de65fd6d1f5ba202efb9b8163456487b51
zhenweili <zhenweili_stu@163.com>
2020-04-10 11:40:06 +0800

    Flink Stream Demo 传参

--- src/main/scala/cn/zwl/wc/StreamWordCount1.scala
+++ src/main/scala/cn/zwl/wc/StreamWordCount1.scala
@@ -1,16 +1,20 @@
 package cn.zwl.wc
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
 
 object StreamWordCount1 {
   def main(args: Array[String]): Unit = {
+    val params=ParameterTool.fromArgs(args)
+    val host:String=params.get("host")
+    val port:Int=params.getInt("port")
 
     //1,创建流处理的执行环境
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
 
     //接受socket数据流
-    val textDataStream=env.socketTextStream("master",7777)
+    val textDataStream=env.socketTextStream(host,port)
 
     //逐一读取数据，打散进行wordcount
 
@@ -20,7 +24,7 @@ object StreamWordCount1 {
       .keyBy(0)
       .sum(1)
 
-    WordCountDataStream.print()
+    WordCountDataStream.print().setParallelism(2)
 
 
     //执行任务
